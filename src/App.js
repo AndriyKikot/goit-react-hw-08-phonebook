015f5724f8c3bfc8318d292cfc27d3391b78bbaa
Andriy Kikot <andray838@gmail.com>
2021-02-05 21:13:57 +0300

    added lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Switch } from 'react-router-dom';
 import AppBar from './components/AppBar';
-import ContactsView from './views/ContactsView';
-import HomeView from './views/HomeView';
-import RegisterView from './views/RegisterView';
-import LoginView from './views/LoginView';
+// import ContactsView from './views/ContactsView';
+// import HomeView from './views/HomeView';
+// import RegisterView from './views/RegisterView';
+// import LoginView from './views/LoginView';
 import Container from './components/Container';
 import { authOperations } from './redux/auth';
 import { authSelectors } from './redux/auth';
@@ -14,11 +14,25 @@ import PublicRoute from './components/PublicRoute';
 
 import './App.css';
 
+const HomeView = lazy(() =>
+  import('./views/HomeView' /* webpackChunkName: "home-view" */),
+);
+
+const RegisterView = lazy(() =>
+  import('./views/RegisterView' /* webpackChunkName: "register-view" */),
+);
+const LoginView = lazy(() =>
+  import('./views/LoginView' /* webpackChunkName: "login-view" */),
+);
+const ContactsView = lazy(() =>
+  import('./views/ContactsView' /* webpackChunkName: "contacts-view" */),
+);
+
 function App() {
+  const dispatch = useDispatch();
   const isFetchingCurrentUser = useSelector(
     authSelectors.getIsFetchingCurrentUser,
   );
-  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
@@ -30,23 +44,25 @@ function App() {
         <Container>
           <AppBar />
 
-          <Switch>
-            <PublicRoute exact path="/">
-              <HomeView />
-            </PublicRoute>
+          <Suspense fallback={'Загружаем'}>
+            <Switch>
+              <PublicRoute exact path="/">
+                <HomeView />
+              </PublicRoute>
 
-            <PublicRoute exact path="/register" restricted>
-              <RegisterView />
-            </PublicRoute>
+              <PublicRoute exact path="/register" restricted>
+                <RegisterView />
+              </PublicRoute>
 
-            <PublicRoute exact path="/login" restricted>
-              <LoginView />
-            </PublicRoute>
+              <PublicRoute exact path="/login" restricted>
+                <LoginView />
+              </PublicRoute>
 
-            <PrivateRoute path="/contacts">
-              <ContactsView />
-            </PrivateRoute>
-          </Switch>
+              <PrivateRoute path="/contacts">
+                <ContactsView />
+              </PrivateRoute>
+            </Switch>
+          </Suspense>
         </Container>
       </div>
     )
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { authSelectors } from '../../redux/auth';
 // import styles from './Navigation.module.css';
 
 const styles = {
@@ -15,21 +17,27 @@ const styles = {
   },
 };
 
-const Navigation = () => (
-  <nav>
-    <NavLink to="/" exact style={styles.link} activeStyle={styles.activeLink}>
-      Главная
-    </NavLink>
+const Navigation = () => {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
-    <NavLink
-      to="/contacts"
-      exact
-      style={styles.link}
-      activeStyle={styles.activeLink}
-    >
-      Контакты
-    </NavLink>
-  </nav>
-);
+  return (
+    <nav>
+      <NavLink to="/" exact style={styles.link} activeStyle={styles.activeLink}>
+        Главная
+      </NavLink>
+
+      {isLoggedIn && (
+        <NavLink
+          to="/contacts"
+          exact
+          style={styles.link}
+          activeStyle={styles.activeLink}
+        >
+          Контакты
+        </NavLink>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
